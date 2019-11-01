b5b1e8f703c78312f365b00c72f26ef846cc2245
guoleiF01 <597358068@qq.com>
2019-11-01 11:29:01 +0800

    add

--- src/main/java/com/itheima/ui/Client.java
+++ src/main/java/com/itheima/ui/Client.java
@@ -1,5 +1,7 @@
 package com.itheima.ui;
 
+import com.itheima.dao.IAccountDao;
+import com.itheima.service.IAccountService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,6 +17,9 @@ public class Client {
         //1 获取核心容器
         ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2根据id获取bean对象
-
+        IAccountService as = (IAccountService)ac.getBean("accountService");
+        IAccountDao adao = ac.getBean("accountDao",IAccountDao.class);
+        System.out.println(as);
+        System.out.println(adao);
     }
 }
